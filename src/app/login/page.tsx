11df0534b7e4ae9724e183a0dfd5e0946dcc46bc
Govind Gupta <guptagovind1410@gmail.com>
2023-08-13 22:24:24 +0530

    Created Signup, Login and ProfilePage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,41 @@
+"use client";
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+// import { axios } from "axios";
+
 export default function loginPage() {
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+      });
+
+    const onLogin = async () => {};
+    
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Login Page</h1>
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="password"
+        id="password"
+        placeholder="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button onClick={onLogin}>Login</button>
+      <Link href="/signup">Don't have an account?</Link>
     </div>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,50 @@
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+// import { axios } from "axios";
+
 export default function signupPage() {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Signup Page</h1>
-        </div>
-    )
+  const [user, setUser] = useState({
+    userName: '',
+    email: '',
+    password: '',
+  });
+
+  const onSignup = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1>Signup Page</h1>
+      <label htmlFor="userName">Username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        id="username"
+        placeholder="username"
+        value={user.userName}
+        onChange={(e) => setUser({ ...user, userName: e.target.value })}
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="text"
+        id="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+        type="password"
+        id="password"
+        placeholder="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button onClick={onSignup}>Sign Up</button>
+      <Link href="/login">Already have an account?</Link>
+    </div>
+  );
 }
